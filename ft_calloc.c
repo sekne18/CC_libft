cdaf979db3f5ec4c93609f85cb53d4a44667c1a6
sekne18 <sekne50@gmail.com>
2024-10-10 18:34:23 +0200

    fixes

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jan <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 07:08:47 by jan               #+#    #+#             */
-/*   Updated: 2024/10/10 16:43:57 by jsekne           ###   ########.fr       */
+/*   Updated: 2024/10/10 18:26:37 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,12 @@
 void	*ft_calloc(size_t nitems, size_t size)
 {
 	unsigned char	*arr;
-	int				i;
 
-	i = -1;
 	if (size == 0 || nitems == 0)
 		arr = malloc(1);
 	else if (nitems > INT_MAX / size)
 		return (NULL);
-	else 
+	else
 		arr = malloc(nitems * size);
 	if (!arr)
 		return (NULL);
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,38 +6,25 @@
 /*   By: jan <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 23:28:28 by jan               #+#    #+#             */
-/*   Updated: 2024/10/10 17:22:22 by jsekne           ###   ########.fr       */
+/*   Updated: 2024/10/10 18:27:18 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-#include <stdio.h>
-	
-int	main(void)
-{
-	char big[] = "aaabcabcd";
-	char little[] = "cd";
-	printf("%s\n", ft_strnstr(big, little, 8));
-	return (0);
-}
-*/
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	const char	*start;
 	const char	*l;
-	const char	*orig;
-	size_t			l_len;
+	size_t		l_len;
 
 	if (ft_strlen(little) == 0)
 		return ((char *)big);
-	orig = big;
 	while (*big && len-- > 0)
 	{
 		start = big;
 		l = little;
-		l_len = len+1;
+		l_len = len + 1;
 		while (*l && *big && *l == *big && l_len-- > 0)
 		{
 			l++;
