d096741e5c4947e18be1440fbc9a5f3f827330f1
Jan Sekne <jsekne@a1-r1-p9.s19.be>
2024-10-10 18:28:24 +0200

    inf loop fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jan <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 18:47:19 by jan               #+#    #+#             */
-/*   Updated: 2024/10/10 15:43:00 by jsekne           ###   ########.fr       */
+/*   Updated: 2024/10/10 18:28:07 by jsekne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ int	ft_atoi(const char *nptr)
 		if (*nptr++ == '-')
 			is_neg *= -1;
 	while (*nptr >= '0' && *nptr <= '9')
-		sum = (sum * 10) + (*nptr - '0');
+		sum = (sum * 10) + (*nptr++ - '0');
 	return (sum * is_neg);
 }
