b5f75abf739c3cf34ec080173fa00813998f8a19
sekne18 <sekne50@gmail.com>
2024-10-10 20:55:21 +0200

    strtrim fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,12 +6,20 @@
 /*   By: jan <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 11:23:57 by jan               #+#    #+#             */
-/*   Updated: 2024/10/06 22:47:42 by jans             ###   ########.fr       */
+/*   Updated: 2024/10/10 20:39:51 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", ft_strncmp("test\200", "test\0", 6));
+	return (0);
+}
+*/
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while ((*s1 || *s2) && n-- > 0)
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,45 +6,36 @@
 /*   By: jan <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:06:22 by jan               #+#    #+#             */
-/*   Updated: 2024/10/06 22:53:27 by jans             ###   ########.fr       */
+/*   Updated: 2024/10/10 20:54:56 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
 #include "libft.h"
 
 int		contains(char const *set, char c);
-char	*ft_strtrim(char const *s1, char const *set);
 
-/*
-int	main()
-{
-	printf("%s\n", ft_strtrim("...JANS....", "."));
-	printf("%s\n", ft_strtrim(".\0...", "\0"));
-	printf("%s\n", ft_strtrim(".  asdasd..", ""));
-	return (0);
-}
-*/
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*new;
 	char	*orig;
 	int		len;
+	int		i;
 
 	len = 0;
 	while (contains(set, *s1))
 		s1++;
 	while (s1[len])
 		len++;
+	len--;
 	while (contains(set, s1[len]))
 		len--;
-	new = malloc((len + 1) * sizeof(char));
+	new = malloc((++len + 1) * sizeof(char));
 	if (!new)
 		return (NULL);
 	orig = new;
-	len = 0;
-	while (s1[len] && !contains(set, s1[len]))
-		*new++ = s1[len++];
+	i = 0;
+	while (i < len)
+		*new++ = s1[i++];
 	*new = '\0';
 	return (orig);
 }
