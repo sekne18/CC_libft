70612732011459c0500e523330a137de7e549e48
sekne18 <sekne50@gmail.com>
2024-10-11 22:42:51 +0200

    all test pass

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,28 +6,19 @@
 /*   By: jan <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:48:22 by jan               #+#    #+#             */
-/*   Updated: 2024/10/11 22:35:37 by jans             ###   ########.fr       */
+/*   Updated: 2024/10/11 22:39:29 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-
-
-int	main()
-{
-	printf("%p\n", ft_memcpy(((void *)0), ((void *)0), 17));
-	printf("%p\n", ft_memcpy("", ((void *)0), 17));
-	return (0);
-}
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
 
-//	if (!src || !dest)
-//		return (NULL);
+	if (!src && !dest)
+		return (NULL);
 	d = dest;
 	s = src;
 	while (n-- > 0)
